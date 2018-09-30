c0306e686b01d27d663ccb51947317005c205578
Rodion Fedotov <fedotov.dev@gmail.com>
2018-09-30 13:06:52 +0500

    boxplot

--- statistics/dispersion.py
+++ statistics/dispersion.py
@@ -1,26 +1,43 @@
-import statistics
-import math
-
-x = int(input())
-values = []
-squares_amount = 0
-amount = 0
-
-while x != 0:
-    amount += x
-    values.append(x)
+def num_input():
+    values = []
     x = int(input())
+    while x != 0:
+        values.append(x)
+        x = int(input())
+    return values
 
-mean = amount / len(values)
+def arifmetic_mean(numbers):
+    return sum(numbers) / len(numbers)
 
-for value in values:
-    squares_amount += (mean - value)**2
+def dispersion(numbers, is_generis):
+    amount = 0
+    mean = arifmetic_mean(numbers)
+    for number in numbers:
+        amount += (mean - number)**2
+    if is_generis:
+        return amount / len(numbers)
+    else:
+        return amount / (len(numbers) - 1)
 
-dispersion = squares_amount / (len(values))
-standart_deviation = dispersion**0.5
+def standart_deviation(numbers, is_generis):
+    return dispersion(numbers, is_generis)**0.5
 
-print("Среднее арифметическое: " + str(mean))
-print("Дисперсия: " + str(dispersion))
-print("Стандартное отклонение: " + str(standart_deviation))
+def find_range(numbers):
+    numbers = sorted(numbers)
+    return numbers[len(numbers)-1] - numbers[0]
 
-print("По библиотеке sd: " + str(math.sqrt(statistics.variance(values))))
+def find_median(numbers):
+    numbers = sorted(numbers)
+    print(numbers)
+    if len(numbers) % 2 == 0:
+        return (numbers[len(numbers)//2 - 1] + numbers[len(numbers)//2]) / 2
+    else:
+        return numbers[len(numbers)//2]
+
+arr = num_input()
+
+print("Среднее арифметическое: " + str(arifmetic_mean(arr)))
+print("Дисперсия: " + str(dispersion(arr, False)))
+print("Стандартное отклонение: " + str(standart_deviation(arr, False)))
+print("Размах: " + str(find_range(arr)))
+print("Медиана: " + str(find_median(arr)))
